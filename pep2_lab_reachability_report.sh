fbfdf46020a12d782db3f852874adb203777ce2e
George Zachos <cse32439@cs.uoi.gr>
2015-06-29 22:21:59 +0300

    Updated scripts after installing DELL workstations @PEP-II

--- pep2_lab_reachability_report.sh
+++ pep2_lab_reachability_report.sh
@@ -12,96 +12,82 @@ NC="\033[0m"    # No-color
 # An initial message is printed to console.
 echo "\n\n ${OR}Checking the reachability of all workstations in \"${CY}PEP-II${OR}\" lab ${NC}"
 
-# "PEP-II" lab is equiped with 10 HP6000 and 18 ULTRA20(SUN) workstations.
-# Their hostnames are hp6000wsXX, with XX in [01,10] and
-# ultra20wsXX, with XX in [01,16]U{40,41}. 
+# "PEP-II" lab is equiped with 25 Dell Optiplex 7020 and 3 Dell Optiplex 7010 workstations.
+# Their hostnames are opti7020wsXX, with XX in [01,25] and opti7010wsXX, with XX in [01,03].
 
-################################# HP6000 #################################
+################################# OPTI7020 #################################
 
-echo "\n\n ${OR}*** HP6000 WORKSTATIONS ***${NC}\n"
+echo "\n\n ${OR}*** OPTI7020 WORKSTATIONS ***${NC}\n"
 
-HP6000_NUM=1
-UNREACHABLE_HP6000=0
+OPTI7020_NUM=1
+UNREACHABLE_OPTI7020=0
 
-# For each hostname from hp6000ws01 to hp6000ws09
-while [ $HP6000_NUM -le 9 ]
+# For each hostname from opti7020ws01 to opti7020ws09
+while [ $OPTI7020_NUM -le 25 ]
 do
-	# The STDOUT stream of the ping command is redirected to /dev/null,
-	# while the STDERR stream is redirected to STDOUT. 
-	# Consequently, nothing is printed to console.
-	# Explaining '2>&1':
-	# '1' is the file descriptor of STDOUT stream and '2' of the STDERR.
-	# '&' indicates that what follows is a file descriptor and not a filename.
-	ping -q -c 2 hp6000ws0$HP6000_NUM > /dev/null 2>&1
-	# if the exit code of the ping command differs from zero(0), the workstation
-	# is unreachable and the appropriate feedback is printed to console.
-	if [ $? -ne 0 ]
+	if [ $OPTI7020_NUM -le 9 ]
 	then
-		echo "${RD} hp6000ws0$HP6000_NUM is unreachable! ${NC}"
-		UNREACHABLE_HP6000=$((UNREACHABLE_HP6000+1))
-	else
-		echo "${GN} hp6000ws0$HP6000_NUM ${NC}"
-	fi
-	HP6000_NUM=$((HP6000_NUM+1))
-done
-
-# Similarly, the reachability test of hp6000ws10 is conducted.
-ping -q -c 2 hp6000ws10 > /dev/null 2>&1
-if [ $? -ne 0 ] 
-then    
-	echo "${RD} hp6000ws10 is unreachable! ${NC}"
-	UNREACHABLE_HP6000=$((UNREACHABLE_HP6000+1))
-else    
-	echo "${GN} hp6000ws10 ${NC}"
-fi      
-
-# The number of the unreachable HP6000 workstations is printed to console.
-echo "\n ${OR}$UNREACHABLE_HP6000 ${CY}HP6000 workstations are unreachable! ${NC}"
-
-################################# ULTRA20 #################################
-
-echo "\n\n ${OR}*** ULTRA20 WORKSTATIONS ***${NC}\n"
-
-ULTRA20_NUM=1
-UNREACHABLE_ULTRA20=0
-
-# For each hostname from ultra20ws01 to ultra20ws16 and for ultra20ws40 & ultra20ws41,
-# the reachability test is conducted in the same way as above.
-while [ $ULTRA20_NUM -le 41 ]
-do
-	if [ $ULTRA20_NUM -le 9 ]
-	then
-		ping -q -c 2 ultra20ws0$ULTRA20_NUM > /dev/null 2>&1
+		# The STDOUT stream of the ping command is redirected to /dev/null,
+		# while the STDERR stream is redirected to STDOUT. 
+		# Consequently, nothing is printed to console.
+		# Explaining '2>&1':
+		# '1' is the file descriptor of STDOUT stream and '2' of the STDERR.
+		# '&' indicates that what follows is a file descriptor and not a filename.
+		ping -q -c 2 opti7020ws0$OPTI7020_NUM > /dev/null 2>&1
+		# if the exit code of the ping command differs from zero(0), the workstation
+		# is unreachable and the appropriate feedback is printed to console.
 		if [ $? -ne 0 ]
-	        then
-        	        echo "${RD} ultra20ws0$ULTRA20_NUM is unreachable! ${NC}"
-                	UNREACHABLE_ULTRA20=$((UNREACHABLE_ULTRA20+1))
-	        else
-        	        echo "${GN} ultra20ws0$ULTRA20_NUM ${NC}"
-        	fi
-	else
-		if [ $ULTRA20_NUM -le 16 ] || [ $ULTRA20_NUM -eq 40 ] || [ $ULTRA20_NUM -eq 41 ]
 		then
-			ping -q -c 2 ultra20ws$ULTRA20_NUM > /dev/null 2>&1
-			if [ $? -ne 0 ]
-                	then
-                        	echo "${RD} ultra20ws$ULTRA20_NUM is unreachable! ${NC}"
-                        	UNREACHABLE_ULTRA20=$((UNREACHABLE_ULTRA20+1))
-                	else
-                        	echo "${GN} ultra20ws$ULTRA20_NUM ${NC}"
-                	fi
+			echo "${RD} opti7020ws0$OPTI7020_NUM is unreachable! ${NC}"
+			UNREACHABLE_OPTI7020=$((UNREACHABLE_OPTI7020+1))
+		else
+			echo "${GN} opti7020ws0$OPTI7020_NUM ${NC}"
+		fi
+	else
+		ping -q -c 2 opti7020ws$OPTI7020_NUM > /dev/null 2>&1
+		if [ $? -ne 0 ]
+		then
+			echo "${RD} opti7020ws$OPTI7020_NUM is unreachable! ${NC}"
+			UNREACHABLE_OPTI7020=$((UNREACHABLE_OPTI7020+1))
+		else
+			echo "${GN} opti7020ws$OPTI7020_NUM ${NC}"
 		fi
 	fi
-	ULTRA20_NUM=$((ULTRA20_NUM+1))
+	OPTI7020_NUM=$((OPTI7020_NUM+1))
 done
 
-# The number of the unreachable ULTRA20 workstations is printed to console.
-echo "\n ${OR}$UNREACHABLE_ULTRA20 ${CY}ULTRA20 workstations are unreachable! ${NC}"
+# The number of the unreachable OPTI7020 workstations is printed to console.
+echo "\n ${OR}$UNREACHABLE_OPTI7020 ${CY}OPTI7020 workstations are unreachable! ${NC}"
+
+################################# OPTI7010 #################################
+
+echo "\n\n ${OR}*** OPTI7010 WORKSTATIONS ***${NC}\n"
+
+OPTI7010_NUM=1
+UNREACHABLE_OPTI7010=0
+
+# For each hostname from opti7010ws01 to opti7010ws03
+# the reachability test is conducted in the same way as above.
+while [ $OPTI7010_NUM -le 3 ]
+do
+	ping -q -c 2 opti7010ws0$OPTI7010_NUM > /dev/null 2>&1
+	if [ $? -ne 0 ]
+	then
+                echo "${RD} opti7010ws0$OPTI7010_NUM is unreachable! ${NC}"
+               	UNREACHABLE_OPTI7010=$((UNREACHABLE_OPTI7010+1))
+	else
+                echo "${GN} opti7010ws0$OPTI7010_NUM ${NC}"
+        fi
+	OPTI7010_NUM=$((OPTI7010_NUM+1))
+done
+
+# The number of the unreachable OPTI7010 workstations is printed to console.
+echo "\n ${OR}$UNREACHABLE_OPTI7010 ${CY}OPTI7010 workstations are unreachable! ${NC}"
 
 ################################# SUMMARY  #################################
 
 # $UNREACHABLE_SUM holds the total number of the unreachable workstations. 
-UNREACHABLE_SUM=$((UNREACHABLE_HP6000+UNREACHABLE_ULTRA20))
+UNREACHABLE_SUM=$((UNREACHABLE_OPTI7020+UNREACHABLE_OPTI7010))
 
 # Finally, $UNREACHABLE_SUM is printed to console.
 echo "\n ${RD}$UNREACHABLE_SUM ${OR}out of the ${CY}28${OR} workstations in \"${CY}PEP-II${OR}\" lab are unreachable! ${NC}"
--- ugrad_lab_reachability_report.sh
+++ ugrad_lab_reachability_report.sh
@@ -95,92 +95,84 @@ echo "\n ${GR}###################### END OF PEP-I REPORT ###################### 
 # An initial message is printed to console.
 echo "\n\n ${OR}Checking the reachability of all workstations in \"${CY}PEP-II${OR}\" lab ${NC}"
 
-# "PEP-II" lab is equiped with 10 HP6000 and 18 ULTRA20(SUN) workstations.
-# Their hostnames are hp6000wsXX, with XX in [01,10] and
-# ultra20wsXX, with XX in [01,16]U{40,41}. 
+# "PEP-II" lab is equiped with 25 Dell Optiplex 7020 and 3 Dell Optiplex 7010 workstations.
+# Their hostnames are opti7020wsXX, with XX in [01,25] and opti7010wsXX, with XX in [01,03].
 
-####################### HP6000 #######################
+################################# OPTI7020 #################################
 
-echo "\n\n ${OR}*** HP6000 WORKSTATIONS ***${NC}\n"
+echo "\n\n ${OR}*** OPTI7020 WORKSTATIONS ***${NC}\n"
 
-HP6000_NUM=1
-UNREACHABLE_HP6000=0
+OPTI7020_NUM=1
+UNREACHABLE_OPTI7020=0
 
-# For each hostname from hp6000ws01 to hp6000ws09
-while [ $HP6000_NUM -le 9 ]
+# For each hostname from opti7020ws01 to opti7020ws09
+while [ $OPTI7020_NUM -le 25 ]
 do
-	ping -q -c 2 hp6000ws0$HP6000_NUM > /dev/null 2>&1
-	# if the exit code of the ping command differs from zero(0), the workstation
-	# is unreachable and the appropriate feedback is printed to console.
-	if [ $? -ne 0 ]
-	then
-		echo "${RD} hp6000ws0$HP6000_NUM is unreachable! ${NC}"
-		UNREACHABLE_HP6000=$((UNREACHABLE_HP6000+1))
-	else
-		echo "${GN} hp6000ws0$HP6000_NUM ${NC}"
-	fi
-	HP6000_NUM=$((HP6000_NUM+1))
+        if [ $OPTI7020_NUM -le 9 ]
+        then
+                # The STDOUT stream of the ping command is redirected to /dev/null,
+                # while the STDERR stream is redirected to STDOUT. 
+                # Consequently, nothing is printed to console.
+                # Explaining '2>&1':
+                # '1' is the file descriptor of STDOUT stream and '2' of the STDERR.
+                # '&' indicates that what follows is a file descriptor and not a filename.
+                ping -q -c 2 opti7020ws0$OPTI7020_NUM > /dev/null 2>&1
+                # if the exit code of the ping command differs from zero(0), the workstation
+                # is unreachable and the appropriate feedback is printed to console.
+                if [ $? -ne 0 ]
+                then
+                        echo "${RD} opti7020ws0$OPTI7020_NUM is unreachable! ${NC}"
+                        UNREACHABLE_OPTI7020=$((UNREACHABLE_OPTI7020+1))
+                else
+                        echo "${GN} opti7020ws0$OPTI7020_NUM ${NC}"
+                fi
+        else
+                ping -q -c 2 opti7020ws$OPTI7020_NUM > /dev/null 2>&1
+                if [ $? -ne 0 ]
+                then
+                        echo "${RD} opti7020ws$OPTI7020_NUM is unreachable! ${NC}"
+                        UNREACHABLE_OPTI7020=$((UNREACHABLE_OPTI7020+1))
+                else
+                        echo "${GN} opti7020ws$OPTI7020_NUM ${NC}"
+                fi
+        fi
+        OPTI7020_NUM=$((OPTI7020_NUM+1))
 done
 
-# Similarly, the reachability test of hp6000ws10 is conducted.
-ping -q -c 2 hp6000ws10 > /dev/null 2>&1
-if [ $? -ne 0 ] 
-then    
-	echo "${RD} hp6000ws10 is unreachable! ${NC}"
-	UNREACHABLE_HP6000=$((UNREACHABLE_HP6000+1))
-else    
-	echo "${GN} hp6000ws10 ${NC}"
-fi      
+# The number of the unreachable OPTI7020 workstations is printed to console.
+echo "\n ${OR}$UNREACHABLE_OPTI7020 ${CY}OPTI7020 workstations are unreachable! ${NC}"
 
-# The number of the unreachable HP6000 workstations in PEP-II lab is printed to console.
-echo "\n ${OR}$UNREACHABLE_HP6000 ${CY}HP6000 workstations are unreachable! ${NC}"
+################################# OPTI7010 #################################
 
-####################### ULTRA20 #######################
+echo "\n\n ${OR}*** OPTI7010 WORKSTATIONS ***${NC}\n"
 
-echo "\n\n ${OR}*** ULTRA20 WORKSTATIONS ***${NC}\n"
+OPTI7010_NUM=1
+UNREACHABLE_OPTI7010=0
 
-ULTRA20_NUM=1
-UNREACHABLE_ULTRA20=0
-
-# For each hostname from ultra20ws01 to ultra20ws16 and for ultra20ws40 & ultra20ws41,
+# For each hostname from opti7010ws01 to opti7010ws03
 # the reachability test is conducted in the same way as above.
-while [ $ULTRA20_NUM -le 41 ]
+while [ $OPTI7010_NUM -le 3 ]
 do
-	if [ $ULTRA20_NUM -le 9 ]
-	then
-		ping -q -c 2 ultra20ws0$ULTRA20_NUM > /dev/null 2>&1
-		if [ $? -ne 0 ]
-	        then
-        	        echo "${RD} ultra20ws0$ULTRA20_NUM is unreachable! ${NC}"
-                	UNREACHABLE_ULTRA20=$((UNREACHABLE_ULTRA20+1))
-	        else
-        	        echo "${GN} ultra20ws0$ULTRA20_NUM ${NC}"
-        	fi
-	else
-		if [ $ULTRA20_NUM -le 16 ] || [ $ULTRA20_NUM -eq 40 ] || [ $ULTRA20_NUM -eq 41 ]
-		then
-			ping -q -c 2 ultra20ws$ULTRA20_NUM > /dev/null 2>&1
-			if [ $? -ne 0 ]
-                	then
-                        	echo "${RD} ultra20ws$ULTRA20_NUM is unreachable! ${NC}"
-                        	UNREACHABLE_ULTRA20=$((UNREACHABLE_ULTRA20+1))
-                	else
-                        	echo "${GN} ultra20ws$ULTRA20_NUM ${NC}"
-                	fi
-		fi
-	fi
-	ULTRA20_NUM=$((ULTRA20_NUM+1))
+        ping -q -c 2 opti7010ws0$OPTI7010_NUM > /dev/null 2>&1
+        if [ $? -ne 0 ]
+        then
+                echo "${RD} opti7010ws0$OPTI7010_NUM is unreachable! ${NC}"
+                UNREACHABLE_OPTI7010=$((UNREACHABLE_OPTI7010+1))
+        else
+                echo "${GN} opti7010ws0$OPTI7010_NUM ${NC}"
+        fi
+        OPTI7010_NUM=$((OPTI7010_NUM+1))
 done
 
-# The number of the unreachable ULTRA20 workstations in PEP-II lab is printed to console.
-echo "\n ${OR}$UNREACHABLE_ULTRA20 ${CY}ULTRA20 workstations are unreachable! ${NC}"
+# The number of the unreachable OPTI7010 workstations is printed to console.
+echo "\n ${OR}$UNREACHABLE_OPTI7010 ${CY}OPTI7010 workstations are unreachable! ${NC}"
 
-################### SUMMARY for PEP-II Lab ###################
+#################### SUMMARY for PEP-II Lab ####################
 
 # $PEP2_UNREACHABLES holds the total number of the unreachable workstations in PEP-II lab. 
-PEP2_UNREACHABLES=$((UNREACHABLE_HP6000+UNREACHABLE_ULTRA20))
+PEP2_UNREACHABLES=$((UNREACHABLE_OPTI7020+UNREACHABLE_OPTI7010))
 
-# $PEP2_UNREACHABLES is printed to console.
+# Finally, $PEP2_UNREACHABLES is printed to console.
 echo "\n ${RD}$PEP2_UNREACHABLES ${OR}out of the ${CY}28${OR} workstations in \"${CY}PEP-II${OR}\" lab are unreachable! ${NC}"
 
 echo "\n ${GR}###################### END OF PEP-II REPORT ###################### ${NC}\n"
